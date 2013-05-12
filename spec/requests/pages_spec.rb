52ba055fd281e1498a18d9d31e338c1672444463
Lee McAlilly <leemcalilly@gmail.com>
2013-05-12 15:14:18 -0400

    adding failing test for the homepage

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe "Pages" do
-  describe "GET /" do
+  
+  describe "Home page" do
+    
     it "displays the homepage" do
       get root_path
       response.status.should be(200)
     end
+    
+    it "has the content" do
+      get root_path
+      page.should have_content("The Fuzz Guide to Guitar")
+    end
+    
   end
+  
 end
