7882a2fcc196b2055ee5a1b05a361770bb0de474
Lee McAlilly <leemcalilly@gmail.com>
2013-05-12 15:21:33 -0400

    adding the home page

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -4,13 +4,13 @@ describe "Pages" do
   
   describe "Home page" do
     
-    it "displays the homepage" do
-      get root_path
-      response.status.should be(200)
+    it "renders the homepage" do
+      visit root_path
+      page.status_code.should be(200)
     end
     
-    it "has the content" do
-      get root_path
+    it "has the right content" do
+      visit root_path
       page.should have_content("The Fuzz Guide to Guitar")
     end
     
