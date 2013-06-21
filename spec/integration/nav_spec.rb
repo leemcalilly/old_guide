6ada90112a27a12368d401b0e5ebaf367a228563
Lee McAlilly <leemcalilly@gmail.com>
2013-06-21 15:32:32 -0400

    tweaking the nav style and removing some files we're not using

--- spec/integration/nav_spec.rb
+++ spec/integration/nav_spec.rb
@@ -5,9 +5,16 @@ describe "Main nav" do
     it "should have the right links in the main nav" do
       visit "/"
       within('#main_nav') do
+        page.should have_link('The Fuzz Guide')
         page.should have_link('Home')
         page.should have_link('Lessons')
       end
+    end
+  end
+  
+  described 'Logged Out' do
+    it "should have the right links in the main nav" do
+      pending
     end
   end
 end
