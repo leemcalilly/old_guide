1262527e7d8544ad7212ccc41b537ddc203c17a9
Lee McAlilly <leemcalilly@gmail.com>
2013-07-16 16:58:28 -0400

    switching S3 to fuzz account

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIJ6HFR3QTBXQJLYQ',
-    :aws_secret_access_key  => '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'
+    :aws_access_key_id      => 'AKIAIEU6H43OR6IK2F4A',
+    :aws_secret_access_key  => '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'
   }  
   
   if Rails.env.test?
@@ -14,8 +14,8 @@ CarrierWave.configure do |config|
   end
   
   if Rails.env.development?
-    config.fog_directory  = 'fuzzguide-development'
+    config.fog_directory  = 'fuzzguide-dev'
   elsif Rails.env.production?
-    config.fog_directory  = 'fuzzguide-production'
+    config.fog_directory  = 'fuzzguide-prod'
   end    
 end
