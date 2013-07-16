99cc6fc51c1954285cd50fdf73fb4037566e6257
Lee McAlilly <leemcalilly@gmail.com>
2013-07-15 22:48:16 -0400

    removing the name field from the photos model

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -62,6 +62,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:name, :image)
+      params.require(:photo).permit(:image)
     end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,3 @@
-class Photo < ActiveRecord::Base
-  validates_presence_of     :name
-  
+class Photo < ActiveRecord::Base  
   mount_uploader :image, ImageUploader
 end
