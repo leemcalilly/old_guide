d11745cbf2fc59a8399e67914cc6c88122d5d18f
Lee McAlilly <leemcalilly@gmail.com>
2013-07-24 22:13:41 -0400

    adding a couple of upload tests

--- spec/integration/photos_spec.rb
+++ spec/integration/photos_spec.rb
@@ -42,8 +42,16 @@ describe "photos" do
   
   describe "Create" do
     describe "admin" do
+      before(:each) do
+        signup_and_login_admin
+        visit '/photos'
+      end
+      
       it "allows admins to upload a photo" do
-        pending
+        attach_file("photo[image]", "#{Rails.root}/spec/support/images/example.jpg")
+        click_button "Upload Photo"
+        current_path == "/photos"
+        page.should have_content("Photo created!")
       end
       
       it "doesn't use /photos/new for uploading" do
@@ -55,7 +63,11 @@ describe "photos" do
       end
       
       it "doesn't allow invalid file types" do
-        pending
+        attach_file("photo[image]", "#{Rails.root}/spec/support/images/example.tiff")
+        click_button "Upload Photo"
+        current_path == "/photos"
+        page.should_not have_content("Photo created!")
+        page.should have_content('You are not allowed to upload "tiff" files, allowed types: jpg, jpeg, gif, png')
       end
       
       it "allows valid file types" do
